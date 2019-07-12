c25a13db8ff4ea28b307cb3261147ffba432b669
Dylan.Zhou <Dylan.Zhou@doctopia.com.cn>
2019-07-12 11:48:22 +0800

    [add] barryvdh/laravel-ide-helper doctrine/dbal

--- app/User.php
+++ app/User.php
@@ -6,9 +6,24 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+/**
+ * App\User
+ *
+ * @property int $id
+ * @property string|null $email
+ * @property string|null $phone
+ * @property string|null $password
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User query()
+ * @mixin \Eloquent
+ */
 class User extends Authenticatable
 {
     use Notifiable;
+
+    protected $table = 'account';
 
     /**
      * The attributes that are mass assignable.
